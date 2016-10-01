6458e35a8aec63bddbc566693de9cf98e5305390
Westporch <westporch@gmail.com>
2016-10-01 12:03:41 +0900

    make menuconfig전 까지 기능 구현

--- kernel_cmp_for_debian.sh
+++ kernel_cmp_for_debian.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# Hyun-gwan Seo
+# Hyun-gwan Seo <westporch@gmail.com>
 
 KERNEL_SOURCE_DIR=/usr/local/src
 KERNEL_VERSION=$1
+KERNEL_SOURCE_BASE=$KERNEL_SOURCE_DIR/linux-$KERNEL_VERSION 
 
 # Parameter check. Only one parameter(kernel version) is required.
     if [ $# -ne 1 ]; then
@@ -14,10 +15,23 @@ KERNEL_VERSION=$1
 # Download kernel source from kernel.org and uncompress it.
 function get_kernel_source()
 {
-    #wget -P $KERNEL_SOURCE_DIR https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.7.6.tar.xz
     wget -P $KERNEL_SOURCE_DIR https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-$KERNEL_VERSION.tar.xz
     unxz $KERNEL_SOURCE_DIR/*.xz
     tar xvf $KERNEL_SOURCE_DIR/*.tar -C $KERNEL_SOURCE_DIR
 }
 
-get__kernel_source    
+# Install required packages for kernel compile.
+function install_required_packages()
+{
+    apt-get install git fakeroot build-essential ncurses-dev xz-utils libssl-dev bc kernel-package -y   # Package size is about 814MB.
+}
+
+# Copy your existing linux kernel config file.
+function copy_existing_kernel_config_file()
+{
+    cp /boot/config-$(uname -r) $KERNEL_SOURCE_BASE/.config
+}
+
+#get_kernel_source    
+#install_required_packages
+#copy_existing_kernel_config_file
